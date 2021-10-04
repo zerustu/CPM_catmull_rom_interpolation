c88f100895c2c159f97351bc4bee3375e5856d4a
zerustu <flareon.hoen@gmail.com>
2021-10-04 16:16:27 +0200

    quickfix 1
    little issue with time calculation

--- animation.js
+++ animation.js
@@ -5,7 +5,7 @@ function animation_catmullrom_rotation(entity,model,frame,total_time,bone,origin
     var anim_moment= entity.getAge()%(total_time*20)
     for (var i=0; i<frame.length-2;i=i+2){
         if (anim_moment>=(frame[i]*20)&&anim_moment<=(frame[i+2]*20)){
-			var t = (anim_moment-(frame[i]*20))/(frame[i+3][0]-frame[i+1][0])
+			var t = (anim_moment-(frame[i]*20))/(frame[i+2]*20-frame[i*20])
             var res_1 = frame[i+1][0][0]*Math.pow(t, 3) + frame[i+1][0][1]*Math.pow(t, 2) + frame[i+1][0][2]*t + frame[i+1][0][3];
             var res_2 = frame[i+1][1][0]*Math.pow(t, 3) + frame[i+1][1][1]*Math.pow(t, 2) + frame[i+1][1][2]*t + frame[i+1][1][3];
             var res_3 = frame[i+1][2][0]*Math.pow(t, 3) + frame[i+1][2][1]*Math.pow(t, 2) + frame[i+1][2][2]*t + frame[i+1][2][3];
@@ -17,7 +17,7 @@ function animation_catmullrom_position(entity,model,frame,total_time,bone,origin
     var anim_moment= entity.getAge()%(total_time*20)
     for (var i=0; i<frame.length-2;i=i+2){
         if (anim_moment>=(frame[i]*20)&&anim_moment<=(frame[i+2]*20)){
-            var t = (anim_moment-(frame[i]*20))/(frame[i+3][0]-frame[i+1][0])
+            var t = (anim_moment-(frame[i]*20))/(frame[i+2]*20-frame[i*20])
             var res_1 = frame[i+1][0][0]*Math.pow(t, 3) + frame[i+1][0][1]*Math.pow(t, 2) + frame[i+1][0][2]*t + frame[i+1][0][3];
             var res_2 = frame[i+1][1][0]*Math.pow(t, 3) + frame[i+1][1][1]*Math.pow(t, 2) + frame[i+1][1][2]*t + frame[i+1][1][3];
             var res_3 = frame[i+1][2][0]*Math.pow(t, 3) + frame[i+1][2][1]*Math.pow(t, 2) + frame[i+1][2][2]*t + frame[i+1][2][3];
